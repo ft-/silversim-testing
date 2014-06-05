ad4df563a5cc802877c30504ceba09c1b71edc3f
ft <ft@noemail>
2014-06-06 01:37:50 +0200

    added #region comments

--- ArribaSim/Stacked/Asset/Handler.cs
+++ ArribaSim/Stacked/Asset/Handler.cs
@@ -1,17 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Reflection;
-using ArribaSim.ServiceInterfaces.Asset;
+﻿/*
+ * ArribaSim is distributed under the terms of the
+ * GNU General Public License v2 
+ * with the following clarification and special exception.
+ * 
+ * Linking this code statically or dynamically with other modules is
+ * making a combined work based on this code. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ * 
+ * As a special exception, the copyright holders of this code give you
+ * permission to link this code with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module. An independent module is a module which is not derived from
+ * or based on this code. If you modify this code, you may extend
+ * this exception to your version of the code, but you are not
+ * obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ * 
+ * License text is derived from GNU classpath text
+ */
+
 using ArribaSim.Main.Common;
+using ArribaSim.ServiceInterfaces.Asset;
 using ArribaSim.Types;
 using ArribaSim.Types.Asset;
-using Nini.Config;
 using log4net;
+using Nini.Config;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace ArribaSim.Stacked.Asset
 {
+    #region Service Implementation
     public class MetadataHandler : AssetMetadataServiceInterface
     {
         private List<AssetServiceInterface> m_Services;
@@ -195,10 +219,10 @@ namespace ArribaSim.Stacked.Asset
             }
         }
         #endregion
-
-
     }
+    #endregion
 
+    #region Factory Implementation
     public class HandlerFactory : IPluginFactory
     {
         private static readonly ILog m_Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
@@ -223,4 +247,5 @@ namespace ArribaSim.Stacked.Asset
             return new Handler(ownSection.Name, new List<string>(services));
         }
     }
+    #endregion
 }
