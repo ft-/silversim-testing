431890692544c7969baa00404a4442b987715c22
ft <ft@noemail>
2014-06-09 14:35:59 +0200

    added missing files

---
+++ ArribaSim/BackendConnectors/Robust/IM/RobustIMConnector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArribaSim.ServiceInterfaces.IM;
+using ArribaSim.Types.IM;
+using ArribaSim.Types;
+using Nwc.XmlRpc;
+using HttpClasses;
+
+namespace ArribaSim.BackendConnectors.Robust.IM
+{
+    public class RobustIMConnector : IMServiceInterface
+    {
+        public int TimeoutMs { get; set; }
+        private static readonly XmlRpcDeserializer m_XmlRpcDeserializer = new XmlRpcDeserializer();
+
+        string m_IMUri;
+
+        public RobustIMConnector(string uri)
+        {
+            TimeoutMs = 20000;
+            m_IMUri = uri;
+        }
+
+        public override void Send(GridInstantMessage im)
+        {
+            Dictionary<string, object> p = new Dictionary<string, object>();
+            p["from_agent_id"] = im.FromAgent.ID.ToString();
+            p["from_agent_session"] = UUID.Zero.ToString();
+            p["to_agent_id"] = im.ToAgent.ID.ToString();
+            p["im_session_id"] = im.IMSessionID.ToString();
+            p["region_id"] = im.RegionID.ToString();
+            p["timestamp"] = im.Timestamp.AsULong.ToString();
+            p["from_agent_name"] = im.FromAgent.FullName;
+            p["from_group"] = im.IsFromGroup ? "TRUE" : "FALSE";
+            p["message"] = im.Message;
+            byte[] v = new byte[1];
+            v[0] = (byte)(int)im.Dialog;
+            p["dialog"] = Convert.ToBase64String(v, Base64FormattingOptions.None);
+            v = new byte[1];
+            v[0] = (im.IsOffline ? (byte)1 : (byte)0);
+            p["offline"] = Convert.ToBase64String(v, Base64FormattingOptions.None);
+            p["parent_estate_id"] = im.ParentEstateID.ToString();
+            p["position_x"] = im.Position.X.ToString();
+            p["position_y"] = im.Position.Y.ToString();
+            p["position_z"] = im.Position.Z.ToString();
+            p["binary_bucket"] = Convert.ToBase64String(im.BinaryBucket, Base64FormattingOptions.None); ;
+
+            List<object> plist = new List<object>();
+            plist.Add(p);
+            XmlRpcRequest req = new XmlRpcRequest("grid_instant_message", plist);
+
+            XmlRpcResponse res = (XmlRpcResponse)m_XmlRpcDeserializer.Deserialize(HttpRequestHandler.DoRequest("POST", m_IMUri, null, "text/xml", req.ToString(), false, TimeoutMs));
+            if(res.IsFault)
+            {
+                throw new IMSendFailedException();
+            }
+            if(res.Value is IDictionary)
+            {
+                IDictionary d = (IDictionary) res.Value;
+                if(bool.Parse(d["success"].ToString()))
+                {
+                    return;
+                }
+            }
+            throw new IMSendFailedException();
+        }
+    }
+}
---
+++ ArribaSim/BackendConnectors/Robust/IM/RobustIMHandler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArribaSim.Main.Common;
+using ArribaSim.Main.Common.HttpServer;
+using ArribaSim.ServiceInterfaces.IM;
+using ArribaSim.Types.IM;
+using Nwc.XmlRpc;
+using log4net;
+using ArribaSim.Types;
+using Nini.Config;
+using System.Threading;
+
+namespace ArribaSim.BackendConnectors.Robust.IM
+{
+    #region Service Implementation
+    class RobustIMHandler : IPlugin
+    {
+        bool m_DisallowOfflineIM;
+        IMServiceInterface m_IMService;
+        public RobustIMHandler(bool disallowOfflineIM)
+        {
+            m_DisallowOfflineIM = disallowOfflineIM;
+        }
+
+        public void Startup(ConfigurationLoader loader)
+        {
+            HttpXmlRpcHandler xmlRpc = loader.GetService<HttpXmlRpcHandler>("XmlRpcServer");
+            xmlRpc.XmlRpcMethods.Add("grid_instant_message", IMReceived);
+            m_IMService = loader.GetService<IMServiceInterface>("IMService");
+        }
+
+        public XmlRpcResponse IMReceived(XmlRpcRequest req)
+        {
+            GridInstantMessage im = new GridInstantMessage();
+            XmlRpcResponse res = new XmlRpcResponse();
+            try
+            {
+                im.NoOfflineIMStore = m_DisallowOfflineIM;
+                IDictionary d = (IDictionary)req.Params[0];
+
+                im.FromAgent.ID = (string)d["from_agent_id"];
+                im.ToAgent.ID = (string)d["to_agent_id"];
+                im.IMSessionID = (string)d["im_session_id"];
+                im.RegionID = (string)d["region_id"];
+                im.Timestamp = Date.UnixTimeToDateTime(ulong.Parse((string)d["timestamp"]));
+                im.FromAgent.FullName = (string)d["from_agent_name"];
+                if(d.Contains("message"))
+                {
+                    im.Message = (string)d["message"];
+                }
+                byte[] dialog = Convert.FromBase64String(d["dialog"].ToString());
+                im.Dialog = (GridInstantMessageDialog)dialog[0];
+                im.IsFromGroup = bool.Parse(d["from_group"].ToString());
+                byte[] offline = Convert.FromBase64String(d["offline"].ToString());
+                im.IsOffline = offline[0] != 0;
+                im.ParentEstateID = (int)d["parent_estate_id"];
+                im.Position.X = float.Parse(d["position_x"].ToString());
+                im.Position.Y = float.Parse(d["position_y"].ToString());
+                im.Position.Z = float.Parse(d["position_z"].ToString());
+                if(d.Contains("binary_bucket"))
+                {
+                    im.BinaryBucket = Convert.FromBase64String(d["binary_bucket"].ToString());
+                }
+            }
+            catch
+            {
+                res.SetFault(-32602, "invalid method parameters");
+                return res;
+            }
+
+            ManualResetEvent e = new ManualResetEvent(false);
+            Dictionary<string, string> p;
+            im.OnResult = delegate(GridInstantMessage _im, bool result) { _im.ResultInfo = result;  e.Set(); };
+            m_IMService.Send(im);
+            try
+            {
+                e.WaitOne(10000);
+            }
+            catch
+            {
+                p = new Dictionary<string,string>();
+                p["result"] = "FALSE";
+                res.Value = p;
+                return res;
+            }
+
+            p = new Dictionary<string, string>();
+            p["result"] = im.ResultInfo ? "TRUE" : "FALSE";
+            res.Value = p;
+
+            return res;
+        }
+    }
+    #endregion
+
+    #region Factory
+    public class RobustIMHandlerFactory : IPluginFactory
+    {
+        public RobustIMHandlerFactory()
+        {
+
+        }
+
+        public IPlugin Initialize(ConfigurationLoader loader, IConfig ownSection)
+        {
+            return new RobustIMHandler(ownSection.GetBoolean("DisallowOfflineIM", true));
+        }
+    }
+    #endregion
+}
