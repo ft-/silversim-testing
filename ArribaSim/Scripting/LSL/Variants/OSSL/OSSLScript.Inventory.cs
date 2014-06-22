bdc2ab57bad2edda3eb1d83866dc3232a64635c4
ft <ft@noemail>
2014-06-22 02:18:56 +0200

    added some OSSL inventory functions

---
+++ ArribaSim/Scripting/LSL/Variants/OSSL/OSSLScript.Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArribaSim.Types;
+
+namespace ArribaSim.Scripting.LSL.Variants.OSSL
+{
+    public partial class OSSLScript
+    {
+        public AString osGetInventoryDesc(AString item)
+        {
+            try
+            {
+                return new AString(Part.Inventory[item.ToString()].Description);
+            }
+            catch
+            {
+                return new AString();
+            }
+        }
+    }
+}
