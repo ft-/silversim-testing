8d517a878bf28b2a02b56c19131e2da3ed8cb0f1
ft <ft@noemail>
2014-08-02 16:59:48 +0200

    reorganized versioning

--- ArribaSim/Scene/ServiceInterfaces/Properties/AssemblyInfo.cs
+++ ArribaSim/Scene/ServiceInterfaces/Properties/AssemblyInfo.cs
@@ -6,20 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ArribaSim.Scene.ServiceInterfaces")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ArribaSim Development")]
 [assembly: AssemblyProduct("ArribaSim.Scene.ServiceInterfaces")]
 [assembly: AssemblyCopyright("Affero GPLv3 License + Link Exception")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("0.8.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
