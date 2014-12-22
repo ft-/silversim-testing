2893d74ac3d88ae6bb72b0fffaba2ae0d3f5c80a
ft <ft@noemail>
2014-12-22 16:02:56 +0100

    added money API template

---
+++ SilverSim/Scripting/LSL/API/Money/Money_API.cs
@@ -0,0 +1,81 @@
+﻿/*
+
+SilverSim is distributed under the terms of the
+GNU Affero General Public License v3
+with the following clarification and special exception.
+
+Linking this code statically or dynamically with other modules is
+making a combined work based on this code. Thus, the terms and
+conditions of the GNU Affero General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this code give you
+permission to link this code with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module. An independent module is a module which is not derived from
+or based on this code. If you modify this code, you may extend
+this exception to your version of the code, but you are not
+obligated to do so. If you do not wish to do so, delete this
+exception statement from your version.
+
+*/
+
+using SilverSim.Main.Common;
+using SilverSim.Scene.Types.Object;
+using SilverSim.Scene.Types.Script;
+using SilverSim.Types;
+using System;
+using SilverSim.Scene.Types.Agent;
+
+namespace SilverSim.Scripting.LSL.API.Money
+{
+    [ScriptApiName("Money")]
+    [LSLImplementation]
+    public partial class Money_API : MarshalByRefObject, IScriptApi, IPlugin
+    {
+        public Money_API()
+        {
+
+        }
+
+        public void Startup(ConfigurationLoader loader)
+        {
+
+        }
+
+        [APILevel(APIFlags.LSL)]
+        public const int PERMISSION_DEBIT = 0x2;
+
+        [APILevel(APIFlags.LSL)]
+        [StateEventDelegate]
+        public delegate void transaction_result(UUID id, int success, string data);
+
+        [APILevel(APIFlags.LSL)]
+        public static void llGiveMoney(ScriptInstance instance, UUID destination, int amount)
+        {
+            Script script = (Script)instance;
+            if((script.m_ScriptPermissions & ScriptPermissions.Debit) == 0 ||
+                script.m_ScriptPermissionsKey != script.Part.Owner.ID ||
+                amount < 0)
+            {
+                return;
+            }
+        }
+
+        [APILevel(APIFlags.LSL)]
+        public static UUID llTransferLindenDollars(ScriptInstance instance, UUID destination, int amount)
+        {
+            Script script = (Script)instance;
+            if ((script.m_ScriptPermissions & ScriptPermissions.Debit) == 0 ||
+                script.m_ScriptPermissionsKey != script.Part.Owner.ID ||
+                amount < 0)
+            {
+                return UUID.Zero;
+            }
+            return UUID.Zero;
+        }
+    }
+}
