67ada2004bc7d25dfc0c1e55001afdc34c1c3e5f
ft <ft@noemail>
2015-04-12 21:40:31 +0200

    added osSetContentType

--- SilverSim/Scripting.LSL/API/HTTP/HTTP.HttpServer.cs
+++ SilverSim/Scripting.LSL/API/HTTP/HTTP.HttpServer.cs
@@ -99,12 +99,80 @@ namespace SilverSim.Scripting.LSL.API.HTTP
             }
         }
 
+        [APILevel(APIFlags.LSL)]
+        public string llGetHTTPHeader(ScriptInstance Instance, UUID requestID, string header)
+        {
+            lock (Instance)
+            {
+                return m_HTTPHandler.GetHttpHeader(requestID, header);
+            }
+        }
+
+        [APILevel(APIFlags.LSL)]
+        public void llHTTPResponse(ScriptInstance Instance, UUID requestID, int status, string body)
+        {
+            lock(Instance)
+            {
+                m_HTTPHandler.HttpResponse(requestID, status, body);
+            }
+        }
+
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_TEXT = 0;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_HTML = 1;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_XML = 2;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_XHTML = 3;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_ATOM = 4;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_JSON = 5;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_LLSD = 6;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_FORM = 7;
+        [APILevel(APIFlags.LSL)]
+        public const int CONTENT_TYPE_RSS = 8;
+
+        [APILevel(APIFlags.LSL)]
+        public void llSetContentType(ScriptInstance Instance, UUID requestID, int contenttype)
+        {
+            lock(Instance)
+            {
+                switch(contenttype)
+                {
+                    case CONTENT_TYPE_TEXT: m_HTTPHandler.SetContentType(requestID, "text/plain"); break;
+                    case CONTENT_TYPE_HTML: m_HTTPHandler.SetContentType(requestID, "text/html"); break;
+                    case CONTENT_TYPE_XML: m_HTTPHandler.SetContentType(requestID, "application/xml"); break;
+                    case CONTENT_TYPE_XHTML: m_HTTPHandler.SetContentType(requestID, "application/xhtml+xml"); break;
+                    case CONTENT_TYPE_ATOM: m_HTTPHandler.SetContentType(requestID, "application/atom+xml"); break;
+                    case CONTENT_TYPE_JSON: m_HTTPHandler.SetContentType(requestID, "application/json"); break;
+                    case CONTENT_TYPE_LLSD: m_HTTPHandler.SetContentType(requestID, "application/llsd+xml"); break;
+                    case CONTENT_TYPE_FORM: m_HTTPHandler.SetContentType(requestID, "application/x-www-form-urlencoded "); break;
+                    case CONTENT_TYPE_RSS: m_HTTPHandler.SetContentType(requestID, "application/rss+xml "); break;
+                    default: m_HTTPHandler.SetContentType(requestID, "text/plain"); break;
+                }
+            }
+        }
+
+        [APILevel(APIFlags.OSSL)]
+        public void osSetContentType(ScriptInstance Instance, UUID id, string type)
+        {
+            lock(Instance)
+            {
+                m_HTTPHandler.SetContentType(id, type);
+            }
+        }
+
         [ExecutedOnScriptReset]
         public static void ResetURLs(ScriptInstance Instance)
         {
             Script script = (Script)Instance;
             lock (script)
             {
+#warning Implement ResetURLs
             }
         }
     }
