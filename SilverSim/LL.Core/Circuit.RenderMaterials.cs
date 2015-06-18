dab6c86cd2b20969ba4792d074abe9177adc7d27
Freaky Tech <ft@noemail>
2015-06-18 12:51:24 +0200

    removed unnecessary using lines

--- SilverSim/LL.Core/Circuit.RenderMaterials.cs
+++ SilverSim/LL.Core/Circuit.RenderMaterials.cs
@@ -33,9 +33,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Xml;
 
 namespace SilverSim.LL.Core
