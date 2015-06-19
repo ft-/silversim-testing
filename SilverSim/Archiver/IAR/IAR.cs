dc6c131c0ff01e75041f8f51b5fdda1c6b29661c
Freaky Tech <ft@noemail>
2015-06-19 16:59:14 +0200

    added parts of IAR logic

---
+++ SilverSim/Archiver/IAR/IAR.cs
@@ -0,0 +1,292 @@
+﻿/*
+
+SilverSim is distributed under the terms of the
+GNU Affero General Public License v3
+with the following clarification and special exception.
+
+Linking this code statically or dynamically with other modules is
+making a combined work based on this code. Thus, the terms and
+conditions of the GNU Affero General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this code give you
+permission to link this code with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module. An independent module is a module which is not derived from
+or based on this code. If you modify this code, you may extend
+this exception to your version of the code, but you are not
+obligated to do so. If you do not wish to do so, delete this
+exception statement from your version.
+
+*/
+
+using SilverSim.Archiver.Tar;
+using SilverSim.Archiver.Common;
+using SilverSim.ServiceInterfaces.Asset;
+using SilverSim.ServiceInterfaces.Inventory;
+using SilverSim.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using SilverSim.Types.Asset;
+using SilverSim.Types.Asset.Format;
+using System.Xml;
+using SilverSim.Types.Inventory;
+using SilverSim.ServiceInterfaces.AvatarName;
+
+namespace SilverSim.Archiver.IAR
+{
+    public static class IAR
+    {
+        public class IARFormatException : Exception
+        {
+            public IARFormatException()
+            {
+
+            }
+        }
+
+        [Flags]
+        public enum LoadOptions
+        {
+            Merge = 0x000000001,
+            NoAssets = 0x00000002
+        }
+
+        public static void Load(
+            UUI principal, 
+            InventoryServiceInterface inventoryService,
+            AssetServiceInterface assetService,
+            AvatarNameServiceInterface nameService,
+            LoadOptions options,
+            string fileName)
+        {
+            TarArchiveReader reader;
+            {
+                FileStream inputFile = new FileStream(fileName, FileMode.Open, FileAccess.Read); 
+                GZipStream gzipStream = new GZipStream(inputFile, CompressionMode.Decompress);
+                reader = new TarArchiveReader(gzipStream);
+            }
+
+            Dictionary<string, UUID> inventoryPath = new Dictionary<string, UUID>();
+
+            for(;;)
+            {
+                TarArchiveReader.Header header;
+                try
+                {
+                    header = reader.ReadHeader();
+                }
+                catch(TarArchiveReader.EndOfTarException)
+                {
+                    return;
+                }
+
+                if(header.FileName.StartsWith("assets/") && (options & LoadOptions.NoAssets) == 0)
+                {
+                    /* Load asset */
+                    AssetData ad = reader.LoadAsset(header, principal);
+                    assetService.Store(ad);
+                }
+
+                if(header.FileName.StartsWith("inventory/"))
+                {
+                    /* Load inventory */
+                    InventoryItem item = LoadInventoryItem(reader, principal, nameService);
+                }
+            }
+        }
+
+        static InventoryItem LoadInventoryItem(
+            Stream s, 
+            UUI principal,
+            AvatarNameServiceInterface nameService)
+        {
+            using (XmlTextReader reader = new XmlTextReader(new ObjectXmlStreamFilter(s)))
+            {
+                for(;;)
+                {
+                    if(!reader.Read())
+                    {
+                        throw new IARFormatException();
+                    }
+
+                    switch(reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if(reader.Name == "InventoryItem")
+                            {
+                                return LoadInventoryItemData(reader, principal, nameService);
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        static InventoryItem LoadInventoryItemData(
+            XmlTextReader reader, 
+            UUI principal,
+            AvatarNameServiceInterface nameService)
+        {
+            InventoryItem item = new InventoryItem();
+            item.Owner = principal;
+
+            for(;;)
+            {
+                if(!reader.Read())
+                {
+                    throw new IARFormatException();
+                }
+
+                switch(reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        switch(reader.Name)
+                        {
+                            case "Name":
+                                item.Name = reader.ReadElementValueAsString();
+                                break;
+
+                            case "ID":
+                                if(!reader.IsEmptyElement)
+                                {
+                                    reader.Skip();
+                                }
+                                break;
+
+                            case "InvType":
+                                item.InventoryType = (InventoryType)reader.ReadElementValueAsInt();
+                                break;
+
+                            case "CreatorUUID":
+                                {
+                                    string text = reader.ReadElementValueAsString();
+                                    UUID uuid;
+                                    if(text.StartsWith("ospa:n="))
+                                    {
+                                        string[] name = text.Substring(7).Split(' ');
+                                        /* OpenSim tag version */
+                                        item.Creator.ID = UUID.Zero;
+                                        item.Creator.FirstName = name[0];
+                                        if(name.Length > 1)
+                                        {
+                                            item.Creator.LastName = name[1];
+                                        }
+
+                                        /* hope that name service knows that avatar */
+                                        try
+                                        {
+                                            item.Creator = nameService[item.Creator.FirstName, item.Creator.LastName];
+                                        }
+                                        catch
+                                        {
+                                            item.Creator = principal;
+                                        }
+                                    }
+                                    else if(UUID.TryParse(text, out uuid))
+                                    {
+                                        item.Creator.ID = uuid;
+                                    }
+                                    else
+                                    {
+                                        item.Creator = principal;
+                                    }
+                                }
+                                break;
+
+                            case "CreatorData":
+                                {
+                                    string creatorData = reader.ReadElementValueAsString();
+                                    try
+                                    {
+                                        item.Creator.CreatorData = creatorData;
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                                break;
+
+                            case "CreationDate":
+                                item.CreationDate = Date.UnixTimeToDateTime(reader.ReadElementValueAsULong());
+                                break;
+
+                            case "Owner":
+                                if(!reader.IsEmptyElement)
+                                {
+                                    reader.Skip();
+                                }
+                                break;
+
+                            case "Description":
+                                item.Description = reader.ReadElementValueAsString();
+                                break;
+
+                            case "AssetType":
+                                item.AssetType = (AssetType)reader.ReadElementValueAsInt();
+                                break;
+
+                            case "SalePrice":
+                                item.SaleInfo.Price = reader.ReadElementValueAsInt();
+                                break;
+
+                            case "SaleType":
+                                item.SaleInfo.Type = (InventoryItem.SaleInfoData.SaleType)reader.ReadElementValueAsUInt();
+                                break;
+
+                            case "BasePermissions":
+                                item.Permissions.Base = (InventoryPermissionsMask)reader.ReadElementValueAsUInt();
+                                break;
+
+                            case "CurrentPermissions":
+                                item.Permissions.Current = (InventoryPermissionsMask)reader.ReadElementValueAsUInt();
+                                break;
+
+                            case "EveryOnePermissions":
+                                item.Permissions.EveryOne = (InventoryPermissionsMask)reader.ReadElementValueAsUInt();
+                                break;
+
+                            case "NextPermissions":
+                                item.Permissions.NextOwner = (InventoryPermissionsMask)reader.ReadElementValueAsUInt();
+                                break;
+
+                            case "Flags":
+                                item.Flags = reader.ReadElementValueAsUInt();
+                                break;
+
+                            case "GroupID":
+                                item.Group.ID = reader.ReadElementValueAsString();
+                                break;
+
+                            case "GroupOwned":
+                                item.IsGroupOwned = reader.ReadElementValueAsBoolean();
+                                break;
+
+                            default:
+                                if(!reader.IsEmptyElement)
+                                {
+                                    reader.Skip();
+                                }
+                                break;
+                        }
+                        break;
+
+                    case XmlNodeType.EndElement:
+                        if(reader.Name != "InventoryItem")
+                        {
+                            throw new IARFormatException();
+                        }
+                        return item;
+                }
+            }
+        }
+    }
+}
