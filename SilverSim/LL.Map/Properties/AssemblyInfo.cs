786de9068fde26c596d241230a2946fd2be731ba
ft <ft@noemail>
2015-07-11 12:07:27 +0200

    added sim wide deletes added missing AssemblyInfo.cs

---
+++ SilverSim/LL.Map/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("LL Viewer Core Protocol Implementation - Map Module")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
