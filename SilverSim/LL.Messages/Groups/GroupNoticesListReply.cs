b12a857f74c2c7843aacd36a392b05d89ce72c9d
ft <ft@noemail>
2015-07-18 13:39:33 +0200

    added missing Groups messages

---
+++ SilverSim/LL.Messages/Groups/GroupNoticesListReply.cs
@@ -0,0 +1,70 @@
+﻿/*
+
+SilverSim is distributed under the terms of the
+GNU Affero General Public License v3
+with the following clarification and special exception.
+
+Linking this code statically or dynamically with other modules is
+making a combined work based on this code. Thus, the terms and
+conditions of the GNU Affero General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this code give you
+permission to link this code with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module. An independent module is a module which is not derived from
+or based on this code. If you modify this code, you may extend
+this exception to your version of the code, but you are not
+obligated to do so. If you do not wish to do so, delete this
+exception statement from your version.
+
+*/
+
+using SilverSim.Types;
+using SilverSim.Types.Asset;
+using System.Collections.Generic;
+
+namespace SilverSim.LL.Messages.Groups
+{
+    [UDPMessage(MessageType.GroupNoticesListReply)]
+    [Reliable]
+    [Trusted]
+    public class GroupNoticesListReply : Message
+    {
+        public UUID AgentID;
+        public UUID GroupID;
+
+        public struct GroupNoticeData
+        {
+            public UUID NoticeID;
+            public Date Timestamp;
+            public string FromName;
+            public string Subject;
+            public bool HasAttachment;
+            public AssetType AssetType;
+        }
+
+        public List<GroupNoticeData> Data = new List<GroupNoticeData>();
+
+        public override void Serialize(UDPPacket p)
+        {
+            p.WriteMessageType(Number);
+            p.WriteUUID(AgentID);
+            p.WriteUUID(GroupID);
+
+            p.WriteUInt8((byte)Data.Count);
+            foreach(GroupNoticeData d in Data)
+            {
+                p.WriteUUID(d.NoticeID);
+                p.WriteUInt32((uint)d.Timestamp.DateTimeToUnixTime());
+                p.WriteStringLen16(d.FromName);
+                p.WriteStringLen16(d.Subject);
+                p.WriteBoolean(d.HasAttachment);
+                p.WriteUInt8((byte)d.AssetType);
+            }
+        }
+    }
+}
