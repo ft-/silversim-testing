dca13afd75bbf02de6c2ad4a41b1074c99849593
ft <ft@noemail>
2015-07-18 10:56:22 +0200

    added classifieds message definitions

---
+++ SilverSim/LL.Messages/Profile/ClassifiedInfoReply.cs
@@ -0,0 +1,80 @@
+﻿/*
+
+SilverSim is distributed under the terms of the
+GNU Affero General Public License v3
+with the following clarification and special exception.
+
+Linking this code statically or dynamically with other modules is
+making a combined work based on this code. Thus, the terms and
+conditions of the GNU Affero General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this code give you
+permission to link this code with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module. An independent module is a module which is not derived from
+or based on this code. If you modify this code, you may extend
+this exception to your version of the code, but you are not
+obligated to do so. If you do not wish to do so, delete this
+exception statement from your version.
+
+*/
+
+using SilverSim.Types;
+using System;
+
+namespace SilverSim.LL.Messages.Profile
+{
+    [UDPMessage(MessageType.ClassifiedInfoReply)]
+    [Reliable]
+    [Trusted]
+    public class ClassifiedInfoReply : Message
+    {
+        public UUID AgentID = UUID.Zero;
+
+        public UUID ClassifiedID = UUID.Zero;
+        public UUID CreatorID = UUID.Zero;
+        public Date CreationDate = new Date();
+        public Date ExpirationDate = new Date();
+        public UInt32 Category = 0;
+        public string Name = string.Empty;
+        public string Description = string.Empty;
+        public UUID ParcelID = UUID.Zero;
+        public UInt32 ParentEstate = 0;
+        public UUID SnapshotID = UUID.Zero;
+        public string SimName = string.Empty;
+        public Vector3 PosGlobal = Vector3.Zero;
+        public string ParcelName = string.Empty;
+        public byte ClassifiedFlags = 0;
+        public int PriceForListing = 0;
+
+        public ClassifiedInfoReply()
+        {
+
+        }
+
+        public override void Serialize(UDPPacket p)
+        {
+            p.WriteMessageType(Number);
+            p.WriteUUID(AgentID);
+            p.WriteUUID(ClassifiedID);
+            p.WriteUUID(CreatorID);
+            p.WriteUInt32((uint)CreationDate.DateTimeToUnixTime());
+            p.WriteUInt32((uint)ExpirationDate.DateTimeToUnixTime());
+            p.WriteUInt32(Category);
+            p.WriteStringLen8(Name);
+            p.WriteStringLen16(Description);
+            p.WriteUUID(ParcelID);
+            p.WriteUInt32(ParentEstate);
+            p.WriteUUID(SnapshotID);
+            p.WriteStringLen8(SimName);
+            p.WriteVector3d(PosGlobal);
+            p.WriteStringLen8(ParcelName);
+            p.WriteUInt8(ClassifiedFlags);
+            p.WriteInt32(PriceForListing);
+        }
+    }
+}
---
+++ SilverSim/LL.Messages/Profile/ClassifiedInfoUpdate.cs
@@ -0,0 +1,73 @@
+﻿/*
+
+SilverSim is distributed under the terms of the
+GNU Affero General Public License v3
+with the following clarification and special exception.
+
+Linking this code statically or dynamically with other modules is
+making a combined work based on this code. Thus, the terms and
+conditions of the GNU Affero General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this code give you
+permission to link this code with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module. An independent module is a module which is not derived from
+or based on this code. If you modify this code, you may extend
+this exception to your version of the code, but you are not
+obligated to do so. If you do not wish to do so, delete this
+exception statement from your version.
+
+*/
+
+using SilverSim.Types;
+using System;
+
+namespace SilverSim.LL.Messages.Profile
+{
+    [UDPMessage(MessageType.ClassifiedInfoUpdate)]
+    [Reliable]
+    [NotTrusted]
+    public class ClassifiedInfoUpdate : Message
+    {
+        public UUID AgentID;
+        public UUID SessionID;
+
+        public UUID ClassifiedID;
+        public UInt32 Category;
+        public string Name;
+        public string Description;
+        public UUID ParcelID;
+        public UInt32 ParentEstate;
+        public UUID SnapshotID;
+        public Vector3 PosGlobal;
+        public byte ClassifiedFlags;
+        public int PriceForListing;
+
+        public ClassifiedInfoUpdate()
+        {
+
+        }
+
+        public static ClassifiedInfoUpdate Decode(UDPPacket p)
+        {
+            ClassifiedInfoUpdate m = new ClassifiedInfoUpdate();
+            m.AgentID = p.ReadUUID();
+            m.SessionID = p.ReadUUID();
+            m.ClassifiedID = p.ReadUUID();
+            m.Category = p.ReadUInt32();
+            m.Name = p.ReadStringLen8();
+            m.Description = p.ReadStringLen16();
+            m.ParcelID = p.ReadUUID();
+            m.ParentEstate = p.ReadUInt32();
+            m.SnapshotID = p.ReadUUID();
+            m.PosGlobal = p.ReadVector3d();
+            m.ClassifiedFlags = p.ReadUInt8();
+            m.PriceForListing = p.ReadInt32();
+            return m;
+        }
+    }
+}
