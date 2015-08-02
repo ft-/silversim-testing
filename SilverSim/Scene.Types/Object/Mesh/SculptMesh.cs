66cbe75d42840018b8062e97f600efa8232476fb
ft <ft@noemail>
2015-08-02 17:38:33 +0200

    added sculpt mesh generation

--- SilverSim/Scene.Types/Object/Mesh/SculptMesh.cs
+++ SilverSim/Scene.Types/Object/Mesh/SculptMesh.cs
@@ -26,6 +26,7 @@ exception statement from your version.
 using CSJ2K;
 using SilverSim.Types;
 using SilverSim.Types.Asset;
+using SilverSim.Types.Primitive;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -57,10 +58,116 @@ namespace SilverSim.Scene.Types.Object.Mesh
             }
         }
 
+        static Vector3 GetVertex(this Bitmap bm, int x, int y, bool mirror)
+        {
+            Vector3 v = new Vector3();
+            System.Drawing.Color c = bm.GetPixel(x, y);
+            if (mirror)
+            {
+                v.X = -(c.R / 255f - 0.5);
+            }
+            else
+            {
+                v.X = c.R / 255f - 0.5;
+            }
+            v.Y = c.G / 255f - 0.5;
+            v.Z = c.B / 255f - 0.5;
+
+            return v;
+        }
+
         internal static Mesh SculptMeshToMesh(this Bitmap bitmap, ObjectPart.PrimitiveShape.Decoded shape)
         {
+            bool mirror = shape.IsSculptMirrored;
+            Mesh mesh = new Mesh();
+            int vertexRowCount = bitmap.Width + 1;
+            bool reverse_horizontal = shape.IsSculptInverted ? !mirror : mirror;
+            PrimitiveSculptType sculptType = shape.SculptType;
 
-            throw new NotImplementedException();
+            /* generate vertex map */
+            for (int y = 0; y <= bitmap.Height; ++y)
+            {
+                for (int x = 0; x <= bitmap.Width; ++x)
+                {
+                    int ax, ay;
+                    ay = y;
+                    if (reverse_horizontal)
+                    {
+                        ax = bitmap.Width - 1 - x;
+                    }
+                    else
+                    {
+                        ax = x;
+                    }
+
+                    if (y == 0)
+                    {
+                        if (sculptType == PrimitiveSculptType.Sphere)
+                        {
+                            ax = bitmap.Width / 2;
+                        }
+                    }
+                    else if (y == bitmap.Height)
+                    {
+                        if (sculptType == PrimitiveSculptType.Torus)
+                        {
+                            ay = 0;
+                        }
+                        else
+                        {
+                            ay = bitmap.Height - 1;
+                        }
+
+                        if (sculptType == PrimitiveSculptType.Sphere)
+                        {
+                            ax = bitmap.Width / 2;
+                        }
+                    }
+
+                    if (x == bitmap.Width)
+                    {
+                        switch (sculptType)
+                        {
+                            case PrimitiveSculptType.Sphere:
+                            case PrimitiveSculptType.Torus:
+                            case PrimitiveSculptType.Cylinder:
+                                ax = 0;
+                                break;
+
+                            default:
+                                ax = bitmap.Width - 1;
+                                break;
+                        }
+                    }
+
+                    Vector3 v = bitmap.GetVertex(ax, ay, mirror);
+                    mesh.Vertices.Add(v);
+                }
+            }
+
+            /* generate triangles */
+            int totalVerticeCount = mesh.Vertices.Count;
+
+            for (int row = 0; row < totalVerticeCount - vertexRowCount; totalVerticeCount += vertexRowCount)
+            {
+                for(int col = 0; col < vertexRowCount - 1; ++col)
+                {
+                    int row2 = row;
+                    Mesh.Triangle tri = new Mesh.Triangle();
+                    tri.VectorIndex0 = row + col;
+                    tri.VectorIndex1 = row + col + 1;
+                    tri.VectorIndex2 = row2 + col + 1;
+                    mesh.Triangles.Add(tri);
+
+                    tri = new Mesh.Triangle();
+                    tri.VectorIndex0 = row + col;
+                    tri.VectorIndex1 = row2 + col;
+                    tri.VectorIndex2 = row2 + col + 1;
+                    mesh.Triangles.Add(tri);
+                }
+            }
+
+            return mesh;
         }
     }
 }
