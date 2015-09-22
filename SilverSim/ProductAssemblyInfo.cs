6f10527393a48acb83b1eca5d83a7c163de365aa
ft <ft@noemail>
2015-09-22 16:24:31 +0200

    designated release-versions specifically as such

--- SilverSim/ProductAssemblyInfo.cs
+++ SilverSim/ProductAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
 
-[assembly: SilverSim.Types.Assembly.InterfaceVersion("1.0.0.0")]
+[assembly: SilverSim.Types.Assembly.InterfaceVersion("0.0.0.0")]
