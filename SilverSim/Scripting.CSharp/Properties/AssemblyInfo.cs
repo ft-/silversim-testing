d9c9178d9062433c8ec5b58d8ea5ec36db0f41ad
ft <ft@noemail>
2015-09-23 17:44:39 +0200

    moved VisualBasic and CSharp specific compilers into silversim-scripting-extra-dev

--- SilverSim/Scripting.CSharp/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// SilverSim is distributed under the terms of the
-// GNU Affero General Public License v3
-using System.Reflection;
-
-[assembly: AssemblyTitle("C# Scripting Implementation")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
