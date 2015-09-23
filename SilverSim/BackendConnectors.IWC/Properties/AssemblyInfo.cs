bebcffceeaa35978a1c1e902bb72ccff7d92f17d
ft <ft@noemail>
2015-09-23 17:28:55 +0200

    moved IWC connector into silversim-iwc-plugin-dev

--- SilverSim/BackendConnectors.IWC/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// SilverSim is distributed under the terms of the
-// GNU Affero General Public License v3
-using System.Reflection;
-
-[assembly: AssemblyTitle("IWC Connectors")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
