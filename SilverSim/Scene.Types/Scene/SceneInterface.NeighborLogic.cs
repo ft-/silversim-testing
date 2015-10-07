c8eedd880f19c9fe2307984a80b2b1492fa04a98
ft <ft@noemail>
2015-10-08 00:30:55 +0200

    added parts of neighbor logic added ignore of same instance neighbors

--- SilverSim/Scene.Types/Scene/SceneInterface.NeighborLogic.cs
+++ SilverSim/Scene.Types/Scene/SceneInterface.NeighborLogic.cs
@@ -5,6 +5,8 @@ using SilverSim.Types.Grid;
 using SilverSim.Http.Client;
 using System.Collections.Generic;
 using System.IO;
+using SilverSim.Types;
+using SilverSim.StructuredData.LLSD;
 
 namespace SilverSim.Scene.Types.Scene
 {
@@ -22,6 +24,12 @@ namespace SilverSim.Scene.Types.Scene
 
         void VerifyNeighbor(RegionInfo rinfo)
         {
+            if(rinfo.ServerURI == RegionData.ServerURI)
+            {
+                /* ignore same instance */
+                return;
+            }
+
             Dictionary<string, string> headers = new Dictionary<string,string>();
             try
             {
@@ -42,6 +50,39 @@ namespace SilverSim.Scene.Types.Scene
             {
                 /* neighbor supports UDP Inter-Sim connects */
             }
+        }
+
+        void EnableSimCircuit(RegionInfo destinationInfo, out UUID sessionID, out uint circuitCode)
+        {
+            Map reqmap = new Map();
+            reqmap["to_region_id"] = destinationInfo.ID;
+            reqmap["from_region_id"] = ID;
+            reqmap["scope_id"] = RegionData.ScopeID;
+            byte[] reqdata;
+            using(MemoryStream ms = new MemoryStream())
+            {
+                LLSD_XML.Serialize(reqmap, ms);
+                reqdata = ms.GetBuffer();
+            }
+
+            IValue iv = LLSD_XML.Deserialize(
+                HttpRequestHandler.DoStreamRequest(
+                "POST", 
+                destinationInfo.ServerURI + "circuit",
+                null,
+                "application/llsd+xml",
+                reqdata.Length,
+                delegate(Stream s)
+                {
+                    s.Write(reqdata, 0, reqdata.Length);
+                },
+                false,
+                10000,
+                null));
+
+            Map resmap = (Map)iv;
+            circuitCode = resmap["circuit_code"].AsUInt;
+            sessionID = resmap["session_id"].AsUUID;
         }
     }
 }
