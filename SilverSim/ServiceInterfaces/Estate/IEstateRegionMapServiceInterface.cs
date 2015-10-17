598f8b122fc1f3cc31cf02135bd96c40f9a505d3
ft <ft@noemail>
2015-10-18 01:12:41 +0200

    add new file IEstateRegionMapServiceInterface.cs

---
+++ SilverSim/ServiceInterfaces/Estate/IEstateRegionMapServiceInterface.cs
@@ -0,0 +1,14 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Types;
+using System.Collections.Generic;
+
+namespace SilverSim.ServiceInterfaces.Estate
+{
+    public interface IEstateRegionMapServiceInterface
+    {
+        List<UUID> this[uint estateID] { get; }
+        uint this[UUID regionID] { get; set; }
+    }
+}
