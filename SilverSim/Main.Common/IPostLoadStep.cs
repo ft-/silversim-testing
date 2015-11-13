c0cb10f73abc2ecb12af7a2f79a36fec0ad7f761
ft <ft@noemail>
2015-11-13 21:06:04 +0100

    added missing IPostLoadStep.cs

---
+++ SilverSim/Main.Common/IPostLoadStep.cs
@@ -0,0 +1,9 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+namespace SilverSim.Main.Common
+{
+    public interface IPostLoadStep : IPlugin
+    {
+    }
+}
