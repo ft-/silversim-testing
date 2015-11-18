7f78464d5c4d8aef8cc1e16fd01a96f2e09104c5
ft <ft@noemail>
2015-11-18 23:25:10 +0100

    added prepend stream header, we lost a few lines by parsing shbangs

--- SilverSim/Scripting.Common/CompilerRegistry.cs
+++ SilverSim/Scripting.Common/CompilerRegistry.cs
@@ -170,53 +170,173 @@ namespace SilverSim.Scripting.Common
                 compiler.SyntaxCheckAndDump(s, user, shbangs, assetID, reader, linenumber);
             }
 
+            public class StreamReaderAddHead : TextReader
+            {
+                TextReader m_InnerReader;
+                string m_Header;
+                public StreamReaderAddHead(string header, TextReader reader)
+                {
+                    m_Header = header;
+                    m_InnerReader = reader;
+                }
+                public override void Close()
+                {
+                    m_InnerReader.Close();
+                }
+                protected override void Dispose(bool disposing)
+                {
+                    if(disposing)
+                    {
+                        m_InnerReader.Dispose();
+                    }
+                }
+                public override int Peek()
+                {
+                    if(m_Header.Length != 0)
+                    {
+                        return (int)m_Header[0];
+                    }
+                    else
+                    {
+                        return m_InnerReader.Read();
+                    }
+                }
+                public override int Read()
+                {
+                    if (m_Header.Length != 0)
+                    {
+                        int c = (int)m_Header[0];
+                        m_Header = m_Header.Substring(1);
+                        return c;
+                    }
+                    else
+                    {
+                        return m_InnerReader.Read();
+                    }
+                }
+                public override int Read(char[] buffer, int index, int count)
+                {
+                    int n = 0;
+                    while(count-- > 0)
+                    {
+                        int c = Read();
+                        if(c < 0)
+                        {
+                            break;
+                        }
+                        buffer[index++] = (char)c;
+                        ++n;
+                    }
+                    return n;
+                }
+
+                public override int ReadBlock(char[] buffer, int index, int count)
+                {
+                    int n = 0;
+                    while (count-- > 0)
+                    {
+                        int c = Read();
+                        if (c < 0)
+                        {
+                            break;
+                        }
+                        buffer[index++] = (char)c;
+                        ++n;
+                    }
+                    return n;
+                }
+
+                public override string ReadLine()
+                {
+                    if(m_Header.Length > 0)
+                    {
+                        string res = string.Empty;
+                        int pos = m_Header.IndexOf('\n');
+                        if(pos >= 0)
+                        {
+                            res = m_Header.Substring(0, pos + 1);
+                            m_Header = m_Header.Substring(pos + 1);
+                            return res;
+                        }
+                        else
+                        {
+                            res = m_Header;
+                            m_Header = string.Empty;
+                        }
+                        return res + m_InnerReader.ReadLine();
+                    }
+                    else
+                    {
+                        return m_InnerReader.ReadLine();
+                    }
+                }
+
+                public override string ReadToEnd()
+                {
+                    return m_Header + m_InnerReader.ReadToEnd();
+                }
+            }
+
             public IScriptAssembly Compile(AppDomain appDom, UUI user, UUID assetID, TextReader reader)
             {
                 int linenumber = 1;
                 Dictionary<int, string> shbangs = new Dictionary<int, string>();
+                string header = string.Empty;
                 while (reader.Peek() == '/')
                 {
                     string shbang = reader.ReadLine();
+                    header += shbang;
                     if (shbang.StartsWith("//#!"))
                     {
                         shbangs.Add(linenumber, shbang);
                     }
-                    ++linenumber;
                 }
 
-                return Compile(user, shbangs, assetID, reader, linenumber);
+                using (StreamReaderAddHead headReader = new StreamReaderAddHead(header, reader))
+                {
+                    return Compile(user, shbangs, assetID, headReader, linenumber);
+                }
             }
 
             public void SyntaxCheck(UUI user, UUID assetID, TextReader reader)
             {
                 int linenumber = 1;
                 Dictionary<int, string> shbangs = new Dictionary<int, string>();
-                while(reader.Peek() == '/')
+                string header = string.Empty;
+                while (reader.Peek() == '/')
                 {
                     string shbang = reader.ReadLine();
-                    if(shbang.StartsWith("//#!"))
+                    header += shbang;
+                    if (shbang.StartsWith("//#!"))
                     {
                         shbangs.Add(linenumber, shbang);
                     }
-                    ++linenumber;
                 }
-                SyntaxCheck(user, shbangs, assetID, reader, linenumber);
+                using (StreamReaderAddHead headReader = new StreamReaderAddHead(header, reader))
+                {
+                    SyntaxCheck(user, shbangs, assetID, headReader, linenumber);
+                }
             }
 
             public void SyntaxCheckAndDump(Stream s, UUI user, UUID assetID, TextReader reader)
             {
                 int linenumber = 1;
                 Dictionary<int, string> shbangs = new Dictionary<int, string>();
+                string header = string.Empty;
                 while (reader.Peek() == '/')
                 {
                     string shbang = reader.ReadLine();
+                    header += shbang;
                     if (shbang.StartsWith("//#!"))
                     {
                         shbangs.Add(linenumber, shbang);
                     }
                     ++linenumber;
                 }
-                SyntaxCheckAndDump(s, user, shbangs, assetID, reader, linenumber);
+                using (StreamReaderAddHead headReader = new StreamReaderAddHead(header, reader))
+                {
+                    SyntaxCheckAndDump(s, user, shbangs, assetID, headReader, linenumber);
+                }
             }
 
         }
