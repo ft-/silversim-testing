01e1c5995ec6df32d7e943b9a98c6e6789765bc8
ft <ft@noemail>
2015-12-06 22:54:29 +0100

    add partial GodUpdateRegionInfo support

---
+++ SilverSim/Viewer.Core/AgentCircuit.UpdateRegionInfo.cs
@@ -0,0 +1,78 @@
+﻿using SilverSim.ServiceInterfaces.Estate;
+using SilverSim.Types.Estate;
+using SilverSim.Types.Grid;
+using SilverSim.Viewer.Messages;
+using SilverSim.Viewer.Messages.Region;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SilverSim.Viewer.Core
+{
+    public partial class AgentCircuit
+    {
+        [PacketHandler(MessageType.GodUpdateRegionInfo)]
+        public void HandleGodUpdateRegionInfo(Message m)
+        {
+            GodUpdateRegionInfo req = (GodUpdateRegionInfo)m;
+            if(req.CircuitAgentID != req.AgentID ||
+                req.CircuitSessionID != req.SessionID)
+            {
+                return;
+            }
+            if(!Agent.IsActiveGod || !Agent.IsInScene(Scene))
+            {
+                return;
+            }
+
+            EstateInfo estate;
+            EstateServiceInterface estateService = Scene.EstateService;
+            uint estateID;
+            if (estateService.RegionMap.TryGetValue(Scene.ID, out estateID) &&
+                estateService.TryGetValue(estateID, out estate))
+            {
+                if (estateID != req.EstateID)
+                {
+                    if (estateService.TryGetValue(req.EstateID, out estate))
+                    {
+                        estateService.RegionMap[Scene.ID] = req.EstateID;
+                    }
+                    else
+                    {
+                        Agent.SendAlertMessage("Unable to change estate", Scene.ID);
+                    }
+                }
+
+                estate.BillableFactor = req.BillableFactor;
+                estate.PricePerMeter = req.PricePerMeter;
+                estateService[estate.ID] = estate;
+            }
+
+            uint regionFlags = req.RegionFlags;
+
+#if SUPPORT_REDIRECT_XY
+            if(req.RedirectGridX != 0)
+            {
+                Scene.RegionData.Location.GridX = (ushort)req.RedirectGridX;
+            }
+
+            if (req.RedirectGridY != 0)
+            {
+                Scene.RegionData.Location.GridX = (ushort)req.RedirectGridY;
+            }
+#endif
+
+#warning GodUpdateRegionInfo.SimName not yet supported
+#warning GodUpdateRegionInfo.RegionFlags & RegionOptionFlags.FixedSun not yet supported
+
+            Scene.RegionSettings.AllowDamage = (regionFlags & (uint)RegionOptionFlags.AllowDamage) != 0;
+            Scene.RegionSettings.BlockTerraform = (regionFlags & (uint)RegionOptionFlags.BlockTerraform) != 0;
+            Scene.RegionSettings.Sandbox = (regionFlags & (uint)RegionOptionFlags.Sandbox) != 0;
+            Scene.RegionSettings.DisableScripts = (regionFlags & (uint)RegionOptionFlags.DisableScripts) != 0;
+            Scene.RegionSettings.DisableCollisions = (regionFlags & (uint)RegionOptionFlags.DisableAgentCollisions) != 0;
+            Scene.RegionSettings.DisablePhysics = (regionFlags & (uint)RegionOptionFlags.DisablePhysics) != 0;
+            Scene.TriggerRegionSettingsChanged();
+        }
+    }
+}
