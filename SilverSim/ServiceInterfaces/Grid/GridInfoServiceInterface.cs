98f1ff85ea1fc5cd7588a4f029f64407a9da58ef
ft <ft@noemail>
2015-12-10 23:57:38 +0100

    added GridInfoServiceInterface

---
+++ SilverSim/ServiceInterfaces/Grid/GridInfoServiceInterface.cs
@@ -0,0 +1,26 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SilverSim.ServiceInterfaces.Grid
+{
+    public abstract class GridInfoServiceInterface
+    {
+        public GridInfoServiceInterface()
+        {
+
+        }
+
+        public abstract string GridNick { get; }
+        public abstract string GridName { get; }
+        public abstract string LoginURI { get; }
+        public abstract string HomeURI { get; }
+        public abstract string this[string key] { get; }
+        public abstract bool ContainsKey(string key);
+        public abstract bool TryGetValue(string key, out string value);
+    }
+}
