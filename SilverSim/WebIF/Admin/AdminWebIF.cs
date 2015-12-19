845db8d41bb15de2c4574bbc5519ec4809ac6b6e
ft <ft@noemail>
2015-12-19 12:52:03 +0100

    add principle structure of WebIF Admin

---
+++ SilverSim/WebIF/Admin/AdminWebIF.cs
@@ -0,0 +1,113 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Main.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nini.Config;
+using SilverSim.Main.Common.HttpServer;
+using SilverSim.ServiceInterfaces.ServerParam;
+using SilverSim.Types;
+using System.Net;
+
+namespace SilverSim.WebIF.Admin
+{
+    #region Service Implementation
+    public class AdminWebIF : IPlugin, IPluginShutdown
+    {
+        ServerParamServiceInterface m_ServerParams;
+        BaseHttpServer m_HttpServer;
+        BaseHttpServer m_HttpsServer;
+
+        public AdminWebIF()
+        {
+        }
+
+        public ShutdownOrder ShutdownOrder
+        {
+            get
+            {
+                return ShutdownOrder.Any;
+            }
+        }
+
+        public void Startup(ConfigurationLoader loader)
+        {
+            m_ServerParams = loader.GetServerParamStorage();
+            m_HttpServer = loader.HttpServer;
+            m_HttpServer.StartsWithUriHandlers.Add("/admin", HandleUnsecureHttp);
+            try
+            {
+                m_HttpsServer = loader.HttpsServer;
+            }
+            catch(ConfigurationLoader.ServiceNotFoundException)
+            {
+                m_HttpsServer = null;
+            }
+
+            if(null != m_HttpsServer)
+            {
+                m_HttpsServer.StartsWithUriHandlers.Add("/admin", HandleHttp);
+            }
+        }
+
+        public void Shutdown()
+        {
+            m_HttpServer.StartsWithUriHandlers.Remove("/admin");
+            if (null != m_HttpsServer)
+            {
+                m_HttpsServer.StartsWithUriHandlers.Remove("/admin");
+            }
+        }
+
+        public void HandleUnsecureHttp(HttpRequest req)
+        {
+            if(null == m_HttpsServer || m_ServerParams.GetBoolean(UUID.Zero, "WebIFAdmin_EnableHTTP", true))
+            {
+                HandleHttp(req);
+            }
+            else
+            {
+                req.ErrorResponse(HttpStatusCode.Forbidden, "Not Allowed");
+            }
+        }
+
+        public void HandleHttp(HttpRequest req)
+        {
+            if(req.RawUrl.StartsWith("/admin/rest"))
+            {
+                if(req.Method != "POST")
+                {
+                    req.ErrorResponse(HttpStatusCode.MethodNotAllowed, "Method not allowed");
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                req.ErrorResponse(HttpStatusCode.NotFound, "Not Found");
+            }
+        }
+    }
+    #endregion
+
+    #region Factory
+    [PluginName("AdminWebIF")]
+    public class AdminWebIFFactory : IPluginFactory
+    {
+        public AdminWebIFFactory()
+        {
+
+        }
+
+        public IPlugin Initialize(ConfigurationLoader loader, IConfig ownSection)
+        {
+            return new AdminWebIF();
+        }
+    }
+    #endregion
+}
