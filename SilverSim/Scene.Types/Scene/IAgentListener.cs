11841312bac96854c319df6bb3ceeff24af05c3a
Freaky Tech <ft@noemail>
2015-12-22 20:50:52 +0100

    added IAgentListener

---
+++ SilverSim/Scene.Types/Scene/IAgentListener.cs
@@ -0,0 +1,14 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Scene.Types.Agent;
+
+namespace SilverSim.Scene.Types.Scene
+{
+    public interface IAgentListener
+    {
+        void AddedAgent(IAgent agent);
+        void AgentChangedScene(IAgent agent);
+        void RemovedAgent(IAgent agent);
+    }
+}
