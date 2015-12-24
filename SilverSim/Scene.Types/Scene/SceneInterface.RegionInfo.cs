eeb646433bbf8d1d24d444838cbb58844580d869
Freaky Tech <ft@noemail>
2015-12-24 15:58:17 +0100

    changed SceneInterface.RegionInfo.cs to use TryGetValue

--- SilverSim/Scene.Types/Scene/SceneInterface.RegionInfo.cs
+++ SilverSim/Scene.Types/Scene/SceneInterface.RegionInfo.cs
@@ -20,20 +20,16 @@ namespace SilverSim.Scene.Types.Scene
         [SuppressMessage("Gendarme.Rules.Exceptions", "DoNotSwallowErrorsCatchingNonSpecificExceptionsRule")]
         internal void HandleRegionHandleRequest(Message m)
         {
-            SilverSim.Viewer.Messages.Region.RegionHandleRequest req = (SilverSim.Viewer.Messages.Region.RegionHandleRequest)m;
-            SilverSim.Viewer.Messages.Region.RegionIDAndHandleReply res;
+            Viewer.Messages.Region.RegionHandleRequest req = (Viewer.Messages.Region.RegionHandleRequest)m;
+            Viewer.Messages.Region.RegionIDAndHandleReply res;
 
-            try
+            RegionInfo ri;
+            if(GridService.TryGetValue(RegionData.ScopeID, out ri))
             {
-                RegionInfo ri = GridService[RegionData.ScopeID, req.RegionID];
                 res = new Viewer.Messages.Region.RegionIDAndHandleReply();
                 res.RegionPosition = ri.Location;
                 res.RegionID = req.RegionID;
                 UDPServer.SendMessageToAgent(req.CircuitAgentID, res);
-            }
-            catch
-            {
-
             }
         }
     }
