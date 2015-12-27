93e000cb7a8442aa39f86ff4bd229c08c1f72cea
Freaky Tech <ft@noemail>
2015-12-27 03:20:36 +0100

    added WebIF console access

---
+++ SilverSim/WebIF/Admin/AdminExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Main.Common;
+using System.Collections.Generic;
+
+namespace SilverSim.WebIF.Admin
+{
+    public static class AdminExtensionMethods
+    {
+        public static AdminWebIF GetAdminWebIF(this ConfigurationLoader loader)
+        {
+            List<AdminWebIF> webIF = loader.GetServicesByValue<AdminWebIF>();
+            if (webIF.Count == 0)
+            {
+                throw new ConfigurationLoader.ConfigurationErrorException("No Admin WebIF service configured");
+            }
+            return webIF[0];
+        }
+
+    }
+}
---
+++ SilverSim/WebIF/Admin/ConsoleAdmin.cs
@@ -0,0 +1,85 @@
+﻿using Nini.Config;
+using SilverSim.Main.Common;
+using SilverSim.Main.Common.CmdIO;
+using SilverSim.Main.Common.HttpServer;
+using SilverSim.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace SilverSim.WebIF.Admin
+{
+    public class ConsoleAdmin : IPlugin
+    {
+        public ConsoleAdmin()
+        {
+
+        }
+
+        public void Startup(ConfigurationLoader loader)
+        {
+
+            AdminWebIF webif = loader.GetAdminWebIF();
+            webif.JsonMethods.Add("console.command", ConsoleCommand);
+        }
+
+        public class ConsoleAdminTty : TTY
+        {
+            StreamWriter m_StreamWriter;
+
+            public ConsoleAdminTty(StreamWriter w)
+            {
+                m_StreamWriter = w;
+            }
+
+            public override void Write(string s)
+            {
+                m_StreamWriter.Write(s);
+            }
+        }
+
+        static readonly UTF8Encoding UTF8NoBOM = new UTF8Encoding(false);
+
+        [AdminWebIF.RequiredRight("console.access")]
+        public void ConsoleCommand(HttpRequest req, Map jsondata)
+        {
+            if(!jsondata.ContainsKey("command"))
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
+                return;
+            }
+
+            string cmd = jsondata["command"].ToString();
+            using (HttpResponse res = req.BeginResponse("text/plain"))
+            {
+                using (Stream o = res.GetOutputStream())
+                {
+                    using (StreamWriter w = new StreamWriter(o, UTF8NoBOM))
+                    {
+                        ConsoleAdminTty tty = new ConsoleAdminTty(w);
+                        CommandRegistry.ExecuteCommand(tty.GetCmdLine(cmd), tty);
+                    }
+                }
+            }
+        }
+    }
+
+    #region Factory
+    [PluginName("ConsoleAdmin")]
+    public class ConsoleAdminFactory : IPluginFactory
+    {
+        public ConsoleAdminFactory()
+        {
+
+        }
+
+        public IPlugin Initialize(ConfigurationLoader loader, IConfig ownSection)
+        {
+            return new ConsoleAdmin();
+        }
+    }
+    #endregion
+}
