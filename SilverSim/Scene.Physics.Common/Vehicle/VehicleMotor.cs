ecec8bfdd9f8895f078f3b0c6f7cf6590de2ff39
Freaky Tech <ft@noemail>
2016-01-04 14:48:25 +0100

    code cleanup

--- SilverSim/Scene.Physics.Common/Vehicle/VehicleMotor.cs
+++ SilverSim/Scene.Physics.Common/Vehicle/VehicleMotor.cs
@@ -1,12 +1,10 @@
 ﻿// SilverSim is distributed under the terms of the
 // GNU Affero General Public License v3
 
-using SilverSim.Scene.Types.Object;
 using SilverSim.Scene.Types.Physics;
 using SilverSim.Scene.Types.Physics.Vehicle;
 using SilverSim.Scene.Types.Scene;
 using SilverSim.Types;
-using System;
 
 namespace SilverSim.Scene.Physics.Common.Vehicle
 {
