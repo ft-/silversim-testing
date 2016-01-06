d21cda949db79dbb9e81dd8ffb9d3c08163d2edc
ft <ft@noemail>
2016-01-06 06:58:33 +0100

    added TelehubInfo packet

---
+++ SilverSim/Viewer.Messages/Telehub/TelehubInfo.cs
@@ -0,0 +1,40 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Types;
+using System.Collections.Generic;
+
+namespace SilverSim.Viewer.Messages.Telehub
+{
+    [UDPMessage(MessageType.TelehubInfo)]
+    [Reliable]
+    [Trusted]
+    public class TelehubInfo : Message
+    {
+        public UUID ObjectID = UUID.Zero;
+        public string ObjectName = string.Empty;
+        public Vector3 TelehubPos = Vector3.Zero;
+        public Quaternion TelehubRot = Quaternion.Identity;
+
+        public List<Vector3> SpawnPoints = new List<Vector3>();
+
+        public TelehubInfo()
+        {
+
+        }
+
+        public override void Serialize(UDPPacket p)
+        {
+            p.WriteMessageType(MessageType.TelehubInfo);
+            p.WriteUUID(ObjectID);
+            p.WriteStringLen8(ObjectName);
+            p.WriteVector3f(TelehubPos);
+            p.WriteLLQuaternion(TelehubRot);
+            p.WriteUInt8((byte)SpawnPoints.Count);
+            foreach(Vector3 spawn in SpawnPoints)
+            {
+                p.WriteVector3f(spawn);
+            }
+        }
+    }
+}
