4cac10620dd24af9b66098be03d184cbd74939e7
ft <ft@noemail>
2016-01-14 23:36:00 +0100

    added TerrainListener for MySQL storage

---
+++ SilverSim/Database.MySQL/SimulationData/MySQLSimulationDataStorage.TerrainListener.cs
@@ -0,0 +1,90 @@
+﻿using MySql.Data.MySqlClient;
+using SilverSim.Types;
+using SilverSim.Viewer.Messages.LayerData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SilverSim.Scene.ServiceInterfaces.SimulationData;
+
+namespace SilverSim.Database.MySQL.SimulationData
+{
+    public partial class MySQLSimulationDataStorage
+    {
+        public class MySQLTerrainListener : TerrainListener
+        {
+            readonly string m_ConnectionString;
+            readonly UUID m_RegionID;
+
+            public MySQLTerrainListener(string connectionString, UUID regionID)
+            {
+                m_ConnectionString = connectionString;
+                m_RegionID = regionID;
+            }
+
+            protected override void StorageTerrainThread()
+            {
+                Thread.CurrentThread.Name = "Storage Terrain Thread: " + m_RegionID.ToString();
+
+                C5.TreeDictionary<uint, uint> knownSerialNumbers = new C5.TreeDictionary<uint, uint>();
+                string replaceIntoTerrain = string.Empty;
+                List<string> updateRequests = new List<string>();
+
+                while (!m_StopStorageThread || m_StorageTerrainRequestQueue.Count != 0)
+                {
+                    LayerPatch req;
+                    try
+                    {
+                        req = m_StorageTerrainRequestQueue.Dequeue(1000);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    uint serialNumber = req.Serial;
+
+                    if (!knownSerialNumbers.Contains(req.ExtendedPatchID) || knownSerialNumbers[req.ExtendedPatchID] != req.Serial)
+                    {
+                        Dictionary<string, object> data = new Dictionary<string, object>();
+                        data["RegionID"] = m_RegionID;
+                        data["PatchID"] = req.ExtendedPatchID;
+                        data["TerrainData"] = req.Serialization;
+                        if (replaceIntoTerrain.Length == 0)
+                        {
+                            replaceIntoTerrain = "REPLACE INTO terrains (" + MySQLUtilities.GenerateFieldNames(data) + ") VALUES ";
+                        }
+                        updateRequests.Add("(" + MySQLUtilities.GenerateValues(data) + ")");
+                    }
+
+                    if((m_StorageTerrainRequestQueue.Count == 0 && updateRequests.Count > 0) || updateRequests.Count >= 256)
+                    {
+                        string elems = string.Join(",", updateRequests);
+                        try
+                        {
+                            using (MySqlConnection conn = new MySqlConnection(m_ConnectionString))
+                            {
+                                conn.Open();
+                                using (MySqlCommand cmd = new MySqlCommand(replaceIntoTerrain + elems, conn))
+                                {
+                                    cmd.ExecuteNonQuery();
+                                }
+                            }
+                            updateRequests.Clear();
+                        }
+                        catch(Exception e)
+                        {
+                            m_Log.Error("Terrain store failed", e);
+                        }
+                    }
+                }
+            }
+        }
+
+        public override TerrainListener GetTerrainListener(UUID regionID)
+        {
+            return new MySQLTerrainListener(m_ConnectionString, regionID);
+        }
+    }
+}
