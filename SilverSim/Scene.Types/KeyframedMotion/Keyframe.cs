3fed59a71602988a3ab2f6b44385f8b9426e66c4
ft <ft@noemail>
2016-01-19 22:24:19 +0100

    commented fields

--- SilverSim/Scene.Types/KeyframedMotion/Keyframe.cs
+++ SilverSim/Scene.Types/KeyframedMotion/Keyframe.cs
@@ -7,7 +7,9 @@ namespace SilverSim.Scene.Types.KeyframedMotion
 {
     public class Keyframe
     {
+        /** <summary>Target position relative from current position</summary> */
         public Vector3 TargetPosition { get; set; }
+        /** <summary>Target rotation relative from current rotation</summary> */
         public Quaternion TargetRotation { get; set; }
         public double Duration { get; set; }
 
