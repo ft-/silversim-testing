2fe988cc20905834e721c0923e495daf61a5fb85
ft <ft@noemail>
2016-01-22 21:11:29 +0100

    using cleanup

--- SilverSim/Database.MySQL/SimulationData/MySQLSimulationDataStorage.TerrainListener.cs
+++ SilverSim/Database.MySQL/SimulationData/MySQLSimulationDataStorage.TerrainListener.cs
@@ -6,10 +6,7 @@ using SilverSim.Types;
 using SilverSim.Viewer.Messages.LayerData;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using SilverSim.Scene.ServiceInterfaces.SimulationData;
 
 namespace SilverSim.Database.MySQL.SimulationData
 {
