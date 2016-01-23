5e3c0d68967407bd8b2237f7a2ffd59aedc98845
ft <ft@noemail>
2016-01-23 20:20:26 +0100

    code cleanup

--- SilverSim/Scene.Types/Physics/PhysicsStateData.cs
+++ SilverSim/Scene.Types/Physics/PhysicsStateData.cs
@@ -3,10 +3,6 @@
 
 using SilverSim.Scene.Types.Object;
 using SilverSim.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace SilverSim.Scene.Types.Physics
 {
