7181dde59ffcae05eff067dc507375197293c010
ft <ft@noemail>
2016-01-24 15:55:33 +0100

    code cleanup

--- SilverSim/Main.Cmd.Region/RegionCommands.cs
+++ SilverSim/Main.Cmd.Region/RegionCommands.cs
@@ -20,7 +20,6 @@ using SilverSim.Types.Parcel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Xml;
