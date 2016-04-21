c371cbe71f545bee8bb13a74de21ce66d23d1c7c
Freaky Tech <ft@noemail>
2016-04-21 15:44:23 +0200

    removed unwanted newline

--- SilverSim/Viewer.Core/AgentCircuit.Capabilities.cs
+++ SilverSim/Viewer.Core/AgentCircuit.Capabilities.cs
@@ -1,5 +1,4 @@
-﻿
-// SilverSim is distributed under the terms of the
+﻿// SilverSim is distributed under the terms of the
 // GNU Affero General Public License v3
 
 using SilverSim.Main.Common.HttpServer;
