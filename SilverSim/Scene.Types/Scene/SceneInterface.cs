3e7054e03f3f9ac3f85307fccadd9315fa920bc4
ft <ft@noemail>
2016-09-03 14:51:25 +0200

    added ExternalHostNameServiceInterface

--- SilverSim/Scene.Types/Scene/SceneInterface.cs
+++ SilverSim/Scene.Types/Scene/SceneInterface.cs
@@ -25,8 +25,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Threading;
 
 namespace SilverSim.Scene.Types.Scene
---
+++ SilverSim/ServiceInterfaces/ExternalHostNameServiceInterface.cs
@@ -0,0 +1,29 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+
+using SilverSim.Threading;
+using System.IO;
+using System.Net;
+
+namespace SilverSim.ServiceInterfaces
+{
+    public abstract class ExternalHostNameServiceInterface
+    {
+        public ExternalHostNameServiceInterface()
+        {
+
+        }
+
+        public abstract string ExternalHostName { get; }
+
+        public string ResolvedIP
+        {
+            get
+            {
+                IPAddress[] addresses = DnsNameCache.GetHostAddresses(ExternalHostName);
+                return addresses[0].ToString();
+            }
+        }
+    }
+}
