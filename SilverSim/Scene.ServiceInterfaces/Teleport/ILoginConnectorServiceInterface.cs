978c2627a6af86c0905026805d64b42ac0dd1356
ft <ft@noemail>
2016-12-04 15:23:49 +0100

    added missing ClientInfo

--- SilverSim/Scene.ServiceInterfaces/Teleport/ILoginConnectorServiceInterface.cs
+++ SilverSim/Scene.ServiceInterfaces/Teleport/ILoginConnectorServiceInterface.cs
@@ -1,7 +1,6 @@
 ﻿// SilverSim is distributed under the terms of the
 // GNU Affero General Public License v3
 
-using SilverSim.Types;
 using SilverSim.Types.Account;
 using SilverSim.Types.Agent;
 using SilverSim.Types.Grid;
@@ -10,6 +9,6 @@ namespace SilverSim.Scene.ServiceInterfaces.Teleport
 {
     public interface ILoginConnectorServiceInterface
     {
-        void LoginTo(UserAccount account, SessionInfo sessionInfo, DestinationInfo destinationInfo, CircuitInfo circuitInfo, AppearanceInfo appearance, TeleportFlags flags);
+        void LoginTo(UserAccount account, ClientInfo clientInfo, SessionInfo sessionInfo, DestinationInfo destinationInfo, CircuitInfo circuitInfo, AppearanceInfo appearance, TeleportFlags flags);
     }
 }
