fb7a70dbb555865e70934097bb4bc1a991a75fad
ft <ft@noemail>
2017-03-11 10:48:05 +0100

    removed starter binary projects

--- SilverSim/Main.Service/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// SilverSim is distributed under the terms of the
-// GNU Affero General Public License v3
-using System.Reflection;
-
-[assembly: AssemblyTitle("Simulator.WindowsService Executable")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
