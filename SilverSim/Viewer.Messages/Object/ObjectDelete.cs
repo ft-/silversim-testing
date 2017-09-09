9a2e9289df71ce38aa6c92ec544bce019844c5fc
ft <ft@noemail>
2017-09-09 19:01:01 +0200

    added three Object message implementations

---
+++ SilverSim/Viewer.Messages/Object/ObjectDelete.cs
@@ -0,0 +1,67 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3 with
+// the following clarification and special exception.
+
+// Linking this library statically or dynamically with other modules is
+// making a combined work based on this library. Thus, the terms and
+// conditions of the GNU Affero General Public License cover the whole
+// combination.
+
+// As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent
+// modules, and to copy and distribute the resulting executable under
+// terms of your choice, provided that you also meet, for each linked
+// independent module, the terms and conditions of the license of that
+// module. An independent module is a module which is not derived from
+// or based on this library. If you modify this library, you may extend
+// this exception to your version of the library, but you are not
+// obligated to do so. If you do not wish to do so, delete this
+// exception statement from your version.
+
+using SilverSim.Types;
+using System;
+using System.Collections.Generic;
+
+namespace SilverSim.Viewer.Messages.Object
+{
+    [UDPMessage(MessageType.ObjectBuy)]
+    [Reliable]
+    [NotTrusted]
+    [Zerocoded]
+    public class ObjectDelete : Message
+    {
+        public UUID AgentID = UUID.Zero;
+        public UUID SessionID = UUID.Zero;
+        public bool Force;
+        public List<UInt32> ObjectLocalIDs = new List<uint>();
+
+        public static Message Decode(UDPPacket p)
+        {
+            var m = new ObjectDelete()
+            {
+                AgentID = p.ReadUUID(),
+                SessionID = p.ReadUUID(),
+                Force = p.ReadBoolean(),
+            };
+            uint c = p.ReadUInt8();
+            for (uint i = 0; i < c; ++i)
+            {
+                m.ObjectLocalIDs.Add(p.ReadUInt32());
+            }
+            return m;
+        }
+
+        public override void Serialize(UDPPacket p)
+        {
+            p.WriteUUID(AgentID);
+            p.WriteUUID(SessionID);
+            p.WriteBoolean(Force);
+            p.WriteUInt8((byte)ObjectLocalIDs.Count);
+            foreach (UInt32 d in ObjectLocalIDs)
+            {
+                p.WriteUInt32(d);
+            }
+        }
+    }
+}
