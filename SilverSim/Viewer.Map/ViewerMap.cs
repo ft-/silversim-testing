2199cb6e77d5a0514b8453284b99736c5f2f2c85
ft <ft@noemail>
2017-09-14 23:45:32 +0200

    adjusted map block handling

--- SilverSim/Viewer.Map/ViewerMap.cs
+++ SilverSim/Viewer.Map/ViewerMap.cs
@@ -313,7 +313,6 @@ namespace SilverSim.Viewer.Map
                         Y = hgLoc.GridY,
                         SizeX = ri.Size.GridX,
                         SizeY = ri.Size.GridY,
-
                         Name = ri.Name,
                         Access = ri.Access,
                         RegionFlags = RegionOptionFlags.None, /* this is same region flags as seen on a sim */
