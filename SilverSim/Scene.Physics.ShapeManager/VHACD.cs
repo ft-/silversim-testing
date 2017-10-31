46a920f2b9e849232ff3c5b623abbc04c3f23504
ft <ft@noemail>
2017-10-31 15:37:58 +0100

    corrected mismatched win dll patch reference

--- SilverSim/Scene.Physics.ShapeManager/VHACD.cs
+++ SilverSim/Scene.Physics.ShapeManager/VHACD.cs
@@ -209,14 +209,14 @@ namespace SilverSim.Scene.Physics.ShapeManager
                         {
                             if (Environment.Is64BitProcess)
                             {
-                                if (IntPtr.Zero == LoadLibrary(Path.Combine(installationBinPath, "platform-libs/windows/64/vhacd.dll")))
+                                if (IntPtr.Zero == LoadLibrary(Path.Combine(installationBinPath, "../platform-libs/windows/64/vhacd.dll")))
                                 {
                                     throw new FileNotFoundException("missing platform-libs/windows/64/vhacd.dll");
                                 }
                             }
                             else
                             {
-                                if (IntPtr.Zero == LoadLibrary(Path.Combine(installationBinPath, "platform-libs/windows/32/vhacd.dll")))
+                                if (IntPtr.Zero == LoadLibrary(Path.Combine(installationBinPath, "../platform-libs/windows/32/vhacd.dll")))
                                 {
                                     throw new FileNotFoundException("missing platform-libs/windows/32/vhacd.dll");
                                 }
