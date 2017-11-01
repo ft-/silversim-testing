9ac537b8489b79c2ea6b365af0f7acfa03fefab7
ft <ft@noemail>
2017-11-01 01:28:06 +0100

    send UseCachedMuteList when no mute list service is available

--- SilverSim/Viewer.MuteList/ViewerMuteListServer.cs
+++ SilverSim/Viewer.MuteList/ViewerMuteListServer.cs
@@ -107,6 +107,7 @@ namespace SilverSim.Viewer.MuteList
 
             if(muteListService == null)
             {
+                circuit.SendMessage(new UseCachedMuteList() { AgentID = agent.ID });
                 return;
             }
 
