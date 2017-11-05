ca5f597ec5de344973e4f7928ea1677dee5fa270
ft <ft@noemail>
2017-11-05 16:04:32 +0100

    revised return value

--- SilverSim/ServiceInterfaces/Economy/EconomyServiceInterface.cs
+++ SilverSim/ServiceInterfaces/Economy/EconomyServiceInterface.cs
@@ -99,7 +99,9 @@ namespace SilverSim.ServiceInterfaces.Economy
         /** <summary>Request script debit permission</summary>
          * this function has to throw exception for signaling error
          */
-        public virtual UUID RequestScriptDebitPermission(UUI sourceID, UUID regionID, UUID objectID, UUID itemID) => UUID.Zero;
+        public virtual void RequestScriptDebitPermission(UUI sourceID, UUID regionID, UUID objectID, UUID itemID)
+        {
+        }
 
         public void ChargeAmount(UUI agentID, BaseTransaction transactionData, int amount, Action processOperation)
         {
