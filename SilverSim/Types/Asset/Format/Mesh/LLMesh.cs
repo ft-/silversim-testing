95d24ffd1dbcedad23479b918055c1e726ef7277
ft <ft@noemail>
2018-01-20 15:13:11 +0100

    add mesh streaming cost calculation

--- SilverSim/Types/Asset/Format/Mesh/LLMesh.cs
+++ SilverSim/Types/Asset/Format/Mesh/LLMesh.cs
@@ -163,5 +163,126 @@ namespace SilverSim.Types.Asset.Format.Mesh
 
             return new MeshLOD(m_AssetData, physOffset, physSize);
         }
+
+        public bool TryGetLODByteSize(LodLevel level, out int bytesize)
+        {
+            bytesize = 0;
+            string lodName;
+            switch (level)
+            {
+                case LodLevel.Physics:
+                    lodName = "physics_mesh";
+                    break;
+
+                case LodLevel.LOD0:
+                    lodName = "lowest_lod";
+                    break;
+
+                case LodLevel.LOD1:
+                    lodName = "low_lod";
+                    break;
+
+                case LodLevel.LOD2:
+                    lodName = "medium_lod";
+                    break;
+
+                case LodLevel.LOD3:
+                    lodName = "high_lod";
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(level));
+            }
+
+            IValue iv;
+            if (!m_MeshData.TryGetValue(lodName, out iv))
+            {
+                return false;
+            }
+
+            var lodMap = iv as Map;
+            if (lodMap == null)
+            {
+                return false;
+            }
+
+            bytesize = lodMap["size"].AsInt;
+
+            if (bytesize < m_EndOfHeader || bytesize <= 0)
+            {
+                bytesize = 0;
+                return false;
+            }
+            return true;
+        }
+
+        private const int MetaDataDiscount = 384;
+        private const int MinimumByteSize = 16;
+        private const int MeshBytesPerTriangle = 16;
+        private const int MeshTriangleBudget = 250000;
+        private const int MaxDistance = 512;
+
+        /* Streaming cost algorithm close to what is known */
+        public double CalculateStreamingCost(Vector3 primscale)
+        {
+            double radius = primscale.Length / 2;
+            double d_lowest = Math.Min(radius / 0.03, MaxDistance);
+            double d_low = Math.Min(radius / 0.06, MaxDistance);
+            double d_mid = Math.Min(radius / 0.24, MaxDistance);
+
+            int lowestLodSize;
+            int lowLodSize;
+            int mediumLodSize;
+            int highLodSize;
+            if(!TryGetLODByteSize(LodLevel.LOD3, out highLodSize))
+            {
+                return 0;
+            }
+            if(!TryGetLODByteSize(LodLevel.LOD2, out mediumLodSize))
+            {
+                mediumLodSize = highLodSize;
+            }
+            if(!TryGetLODByteSize(LodLevel.LOD1, out lowLodSize))
+            {
+                lowLodSize = mediumLodSize;
+            }
+            if(!TryGetLODByteSize(LodLevel.LOD0, out lowestLodSize))
+            {
+                lowestLodSize = lowLodSize;
+            }
+
+            double tri_lowest = Math.Max((double)lowestLodSize - MetaDataDiscount, MinimumByteSize) / MeshBytesPerTriangle;
+            double tri_low = Math.Max((double)lowLodSize - MetaDataDiscount, MinimumByteSize) / MeshBytesPerTriangle;
+            double tri_medium = Math.Max((double)mediumLodSize - MetaDataDiscount, MinimumByteSize) / MeshBytesPerTriangle;
+            double tri_high = Math.Max((double)highLodSize - MetaDataDiscount, MinimumByteSize) / MeshBytesPerTriangle;
+
+            double max_area = 102944;
+            double min_area = 1;
+
+            double high_area = Math.Min(Math.PI * d_mid * d_mid, max_area);
+            double medium_area = Math.Min(Math.PI * d_low * d_low, max_area);
+            double low_area = Math.Min(Math.PI * d_lowest * d_lowest, max_area);
+            double lowest_area = max_area;
+
+            lowest_area -= low_area;
+            low_area -= medium_area;
+            medium_area -= high_area;
+
+            high_area = high_area.Clamp(min_area, max_area);
+            medium_area = medium_area.Clamp(min_area, max_area);
+            low_area = low_area.Clamp(min_area, max_area);
+            lowest_area = lowest_area.Clamp(min_area, max_area);
+
+            double total_area = high_area + medium_area + lowest_area + lowest_area;
+            high_area /= total_area;
+            medium_area /= total_area;
+            low_area /= total_area;
+            lowest_area /= total_area;
+
+            return (tri_high * high_area +
+                tri_medium * medium_area *
+                tri_low * low_area +
+                tri_lowest * lowest_area) / MeshTriangleBudget;
+        }
     }
 }
