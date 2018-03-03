024433d4e0fff21e09ffc2ea3bea85b852b4b819
ft <ft@noemail>
2018-03-03 18:49:24 +0100

    have to move the CurrentLineNumber setting to a better place

--- SilverSim/Scripting.Common/ParserBase.cs
+++ SilverSim/Scripting.Common/ParserBase.cs
@@ -323,7 +323,6 @@ namespace SilverSim.Scripting.Common
 
             for(;;)
             {
-                CurrentLineNumber = cur_linenumber;
                 int c = pi.Reader.Read();
                 if(c == -1)
                 {
