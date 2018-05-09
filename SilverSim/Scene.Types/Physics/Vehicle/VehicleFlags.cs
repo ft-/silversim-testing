3efd95645fecc0a4d86e6cf7cbab8f7d24da6634
ft <ft@noemail>
2018-05-09 21:07:36 +0200

    comment a few of the flags

--- SilverSim/Scene.Types/Physics/Vehicle/VehicleFlags.cs
+++ SilverSim/Scene.Types/Physics/Vehicle/VehicleFlags.cs
@@ -27,20 +27,33 @@ namespace SilverSim.Scene.Types.Physics.Vehicle
     public enum VehicleFlags : uint
     {
         None = 0,
+        /** <summary>Prevent linear Z deflection on vehicle</summary> */
         NoDeflectionUp = 0x0001,
+        /** <summary>Limit vertical attractor to roll</summary> */
         LimitRollOnly = 0x0002,
+        /** <summary>Only hover above water height. Ignore terrain height.</summary> */
         HoverWaterOnly = 0x0004,
+        /** <summary>Only hover above terrain height. Ignore water height.</summary> */
         HoverTerrainOnly = 0x0008,
+        /** <summary>Hover at global height. Ignore water and terrain height.</summary> */
         HoverGlobalHeight = 0x0010,
+        /** <summary>Hover pushes only up. It does not push down.</summary> */
         HoverUpOnly = 0x0020,
+        /** <summary>Limit motor direction vector from lifting the vehicle into sky.</summary> */
         LimitMotorUp = 0x0040,
+        /** <summary>Steer the vehicle using the mouse. Angular motor follows client camera direction.</summary> */
         MouselookSteer = 0x0080,
+        /** <summary>Similar to MouselookSteer except that the left-right motion of camera is mapped to roll angle</summary> */
         MouselookBank = 0x0100,
+        /** <summary>Make mouselook camera rotating independently of vehicle</summary> */
         CameraDecoupled = 0x0200,
 
         /* halcyon based extensions */
+        /** <summary>vehicle reacts to water currents</summary> */
         ReactToCurrents = 0x10000,
+        /** <summary>vehicle reacts to wind force</summary> */
         ReactToWind = 0x20000,
+        /** <summary>Limit motor dirction vector from pushing the vehicle down</summary> */
         LimitMotorDown = 0x40000,
         TorqueWorldZ = 0x80000,
         MousePointSteer = 0x100000,
