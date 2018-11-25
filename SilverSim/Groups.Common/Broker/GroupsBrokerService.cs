26186b3168ce27425168bc14ff3f17c80f8f3cd7
ft <ft@noemail>
2018-11-25 12:36:55 +0100

    code cleanup

--- SilverSim/Groups.Common/Broker/GroupsBrokerService.cs
+++ SilverSim/Groups.Common/Broker/GroupsBrokerService.cs
@@ -28,7 +28,6 @@ using SilverSim.ServiceInterfaces.Groups;
 using SilverSim.ServiceInterfaces.UserAgents;
 using SilverSim.Threading;
 using SilverSim.Types;
-using SilverSim.Types.ServerURIs;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
